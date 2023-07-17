7c03e1e82cd9c3b9853de1f1d85b2422bf5c82a3
imrannn24 <imran200424@icloud.com>
2023-07-17 15:08:33 +0600

    dz

---
+++ SwiftUIProject/CustomColors.swift
@@ -0,0 +1,19 @@
+//
+//  CustomColors.swift
+//  SwiftUIProject
+//
+//  Created by imran on 10.07.2023.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct CustomColor {
+    static let backColor: CGColor = #colorLiteral(red: 0.9568627477, green: 0.9568627477, blue: 0.9568627477, alpha: 1)
+    
+    static let priceColor: CGColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+    
+    static let pink: CGColor = #colorLiteral(red: 0.7340224981, green: 0.2998719513, blue: 0.7803224921, alpha: 1)
+
+}
